1b8463b6079aafe07677f6ef8f908a00441177da
George Flauta <george.flauta@colorado.edu>
2017-06-29 14:47:52 -0600

    :sparkles: added multiview

--- public/app.config.js
+++ public/app.config.js
@@ -25,11 +25,6 @@
                     }
                 }
             })
-            .state('home.child', {
-                url: 'child/',
-                component: 'child'
-              })
-
     }
 
 }())
--- public/child/child.component.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    angular.module('app')
-        .component('child', {
-            controller: console.log('child controller is working'),
-            templateUrl: '/child/child.html'
-        })
-})()
